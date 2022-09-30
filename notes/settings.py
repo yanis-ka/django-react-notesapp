9c1e2f2674c21dad6ca599e1180456254ca5ea7d
yanis khellaf <yanis.khellaf@outlook.fr>
2022-09-30 15:15:35 +0200

    heroku settings

--- notes/settings.py
+++ notes/settings.py
@@ -155,16 +155,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
-if os.environ.get('ENV') == 'PRODUCTION':
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
+# if os.environ.get('ENV') == 'PRODUCTION':
+#     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#     STATICFILES_DIRS = (
+#         os.path.join(PROJECT_ROOT, 'static'),
+#     )
 
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#     # Simplified static file serving.
+#     # https://warehouse.python.org/project/whitenoise/
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
